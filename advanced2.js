dc2bf8cd4bb4840f2f74bffb92072a60efdafc87
Kimberley Blankenhorn <kvandlen@yahoo.com>
2020-09-17 15:13:51 +0200

    array finish

--- advanced2.js
+++ advanced2.js
@@ -5,4 +5,9 @@ var a = [[1, 2, 1, 24],
 		[3, 10, 25, 7], 
 		[21, 4, 6, 17], 
 		[3, 5, 26, 3]];
-console.log(a[1][2]);
+console.log(a[1][1]);
+console.log(a[4][2]);
+console.log(a[5][3]);
+console.log(a[2][3]);
+console.log(a[2][1]);
+
