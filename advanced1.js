93fc22aaa42341abe4c5154e531ed20a77cea2b7
Christian00000 <c.froemmer@gmail.com>
2020-09-17 14:49:07 +0200

    advanced 1,2,3 solved togehter

---
+++ advanced1.js
@@ -0,0 +1,28 @@
+var a = 7;
+console.log(a);
+var b = 14;
+console.log(b);
+var c = 21;
+console.log(c);
+var d = 36;
+console.log(d);
+var e = 42;
+console.log(e);
+
+var Z = a + b + c + d + e;
+console.log(Z);
+
+var f = 1;
+
+var g = 15;
+
+var h = 8;
+
+var i = 1;
+
+var Y = f * g * h * i;
+console.log(Y)
+
+var A = Y/Z;
+console.log(A);
+document.write(A);
